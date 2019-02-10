fcd7299659d81c3573eb48cd985a7d2e6899e6f0
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2019-02-10 17:31:49 +0100

    initial directive spec

---
+++ src/pesel/pesel.directive.spec.ts
@@ -0,0 +1,8 @@
+import { PeselValidatorDirective } from "./pesel.directive";
+
+describe("PeselValidatorDirective", () => {
+  it("should create an instance", () => {
+    const directive = new PeselValidatorDirective();
+    expect(directive).toBeTruthy();
+  });
+});
