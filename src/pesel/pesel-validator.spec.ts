7a657dd22cfea54e4bf2159ec5fd87413a5967ce
Kamil Pius <kamilpius.waw@gmail.com>
2021-07-05 10:58:29 +0200

    pesel validator function, no dependency, more test cases

--- src/pesel/pesel-validator.spec.ts
+++ src/pesel/pesel-validator.spec.ts
@@ -5,21 +5,33 @@ describe('PESEL validator', () => {
   it('should work for empty control', () => {
     const control: FormControl = new FormControl('');
     const validated = peselValidator(control);
-    expect(validated).toBeUndefined();
+    expect(validated).toBeNull();
+  });
+
+  it('should work for null control', () => {
+    const control: FormControl = new FormControl(null);
+    const validated = peselValidator(control);
+    expect(validated).toBeNull();
+  });
+
+  it('should work for undefined control', () => {
+    const control: FormControl = new FormControl(undefined);
+    const validated = peselValidator(control);
+    expect(validated).toBeNull();
   });
 
   it('should work for valid PESEL', () => {
-    const pesels = ['49040501580', '74021834018', '74021834025', '74021834001'];
+    const pesels = ['49040501580', '74021834018', '74021834025', '74021834001', 74021834001];
 
     pesels.forEach(pesel => {
       const control: FormControl = new FormControl(pesel);
       const validated = peselValidator(control);
-      expect(validated).toBeUndefined();
+      expect(validated).toBeNull();
     });
   });
 
   it('should work for invalid pesel', () => {
-    const pesels = ['00000000000', '74021834012'];
+    const pesels = ['00000000000', '74021834012', 74021834012, 1001, 'PESEL'];
 
     pesels.forEach(pesel => {
       const control: FormControl = new FormControl(pesel);
